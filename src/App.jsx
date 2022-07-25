013a31bc2e0f35827ebdcd7ac06246268a20c5c8
Daniela Martinez <rene2.martinez@outlook.com>
2022-07-24 21:19:26 -0700

    quote container

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import quotes from "./json/data.json";
+import color from "./utils/colors";
+import Quotes from './components/Quotes';
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(quotes);
+
+  const getRandomElement = (arr) => {
+    let random = Math.floor(Math.random() * arr.length)
+    return arr[random];
+  }
+
+  let randomQuote = getRandomElement(quotes);
+  let randomColor = getRandomElement(color)
+
+  const [colorRandom, setcolorRandom] = useState(randomColor)
+  const [Quote, setQuote] = useState(randomQuote)
+
+  const changeQuote = () => {
+    randomQuote = getRandomElement(quotes);
+    randomColor = getRandomElement(color)
+
+    setcolorRandom(randomColor)
+    setQuote(randomQuote)
+  }
+
+  const objBgColor = {
+    backgroundColor: colorRandom
+  }
 
   return (
-    <div className="App">
-      <h1>hola</h1>
+    <div style={objBgColor} className="App">
+      <Quotes 
+      randomQuotes={Quote}
+      randomColor={colorRandom}
+      changeQuote={changeQuote} />
     </div>
   )
 }
--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const Quotes = () => {
+const Quotes = ({randomQuotes, randomColor, changeQuote}) => {
+    const objColor = {
+        color: randomColor
+    }
   return (
-    <div>Quotes</div>
+    <div className='quotes-container'>
+        <p className='quote' style={objColor}> <i className='quote-icon' class="fa-solid fa-quote-left"></i> {randomQuotes.quote}</p>
+        <div className='btn-container'>
+            <p className='author' style={objColor}>{randomQuotes.author}</p>
+            <button onClick={changeQuote} style={{backgroundColor: randomColor}}><i class="fa-solid fa-shuffle"></i></button>
+        </div>
+    </div>
   )
 }
 
